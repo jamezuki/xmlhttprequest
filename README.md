1e3528489cc31ccc9712d4a4786e351ef7aa8fc8
Jamezuki <jamezuki@gmail.com>
2021-05-30 19:07:37 -0500

    Squash

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# Sass template
+# AJAX - XMLHttpRequest
+Use of GitHub API https://api.github.com/users to get first 30 users.
 
-1. Install Sass if you haven't
-```
-npm install -g sass
-```
-2. Create package.json 
-```
-npm init
-```
-3. Add script to package.json
-```
-"sass": "sass --style=compressed --no-source-map --watch styles/sass/master.scss styles/master.css"
-```
-4. Compile with:
-```
-npm run sass
-```
+**[Live Project](https://jamezuki.github.io/xmlhttprequest/)**
+
+## Topics
+
+* SASS
+* CSS Flex
+* DOM Manipulation
+* Event Listeners
+* XMLHttpRequest
+* Consume API
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,54 @@
-console.log('Hello World!');
+document.querySelector('button').addEventListener('click', getUsers);
+
+function getUsers() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users', true);
+
+    // OPTIONAL
+    console.log('READYSTATE: ', xhr.readyState);
+
+    // OPTIONAL - used for loaders
+    xhr.onprogress = function () {
+        console.log('READYSTATE: ', xhr.readyState);
+    }
+
+    xhr.onload = function () {
+        console.log('READYSTATE: ', xhr.readyState);
+        if (this.status == 200) {
+            let users = JSON.parse(this.responseText);
+            let output = '';
+            for (let i in users) {
+                output += `
+                    <div class="user">
+                        <img src="${users[i].avatar_url}" width="70" height="70">
+                        <ul>
+                            <li>ID: ${users[i].id}</li>
+                            <li>Login: ${users[i].login}</li>
+                        </ul>
+                    </div>`;
+            }
+            document.querySelector('#users').innerHTML = output;
+        } else if (this.status = 404) {
+            document.querySelector('#users').innerHTML = 'Not Found';
+        }
+    }
+
+    xhr.onerror = function () {
+        console.log('Request Error...');
+        document.querySelector('#users').innerHTML = 'Request Error...';
+    }
+
+    xhr.send();
+
+    // readyState Values
+    // 0: request not initialized 
+    // 1: server connection established
+    // 2: request received 
+    // 3: processing request 
+    // 4: request finished and response is ready
+
+    // HTTP Statuses
+    // 200: "OK"
+    // 403: "Forbidden"
+    // 404: "Not Found"
+}
